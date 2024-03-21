1ff21def7025cfd73c93ce272d9ee254568234a6
YUI <1013094250yui@gmail.com>
2024-03-20 20:24:47 -0400

    Refator

--- server.js
+++ server.js
@@ -1,7 +1,22 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
 
 dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<password>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+  })
+  .then(() => console.log('DB connection sucessful'));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
